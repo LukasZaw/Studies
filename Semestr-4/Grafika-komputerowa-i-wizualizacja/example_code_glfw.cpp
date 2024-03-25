f643c5e7f8dac93dc235ba31111066c24ba63dcd
Łukasz <lukasz.zawodz@gmail.com>
2024-03-25 20:58:56 +0100

    Grafika Lab04 setup

---
+++ Semestr-4/Grafika-komputerowa-i-wizualizacja/example_code_glfw.cpp
@@ -0,0 +1,85 @@
+#include <GLFW/glfw3.h> 
+#include<iostream> 
+
+using namespace std; 
+
+void display(float size) {
+    glColor3d(1.0, 0.0, 0.0);
+
+    glBegin(GL_QUADS);
+    glVertex3f(-20.0, -20.0, 0.0); 
+    glVertex3f(20.0, -20.0, 0.0);  
+    glVertex3f(20.0, 20.0, 0.0);   
+    glVertex3f(-20.0, 20.0, 0.0);  
+    glEnd();
+}
+
+const GLfloat light_ambient[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+
+const GLfloat mat_ambient[] = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
+
+int main(void)
+{
+    GLFWwindow* window; 
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(800, 600, "Przykladowe Okno GLFW", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity(); 
+        glOrtho(-width / 2.0, width / 2.0, -height / 2.0, height / 2.0, -10.0, 10.0);
+
+        glMatrixMode(GL_MODELVIEW);
+
+        float squareSize = static_cast<float>(std::min(width, height)) * 0.2f;
+
+        display(squareSize);
+
+	glEnable(GL_CULL_FACE);
+        glClearColor(1, 1, 1, 1);
+        glfwSwapBuffers(window);
+
+        glEnable(GL_LIGHT0);
+        glEnable(GL_NORMALIZE);
+        glEnable(GL_COLOR_MATERIAL);
+        glEnable(GL_LIGHTING);
+
+        glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+        glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+        glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+        glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0; 
+}
