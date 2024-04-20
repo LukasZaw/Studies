a15cb6fec11c7b72f4fb406525914ac4ebeabc1d
Łukasz <lukasz.zawodz@gmail.com>
2024-04-20 23:03:58 +0200

    niskopoziomowe lab07 CMOVcc

--- Semestr-4/Programowanie-niskopoziomowe/Lab07_CMOVcc/Lab07_CMOVcc.cpp
+++ Semestr-4/Programowanie-niskopoziomowe/Lab07_CMOVcc/Lab07_CMOVcc.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 extern "C" void zad0(INT64* v1, INT64* v2, INT64* v3, INT64* v4, int n);
 extern "C" __int64 zad1(int, int);
+extern "C" __int64 zad2(int, int, int);
+extern "C" __int64 zad3(int, int);
+extern "C" __int64 zad4(int**, int);
+extern "C" __int64 zad5(int, int);
+
+
 
 int main()
 {
@@ -17,8 +23,42 @@ int main()
     zad0(v1, v2, v3, v4, N);
     for (int i = 0; i < N; i++)
         cout << v4[i] << ", ";
+    cout << endl;
     
-    cout << "Zadanie 1\n";
-    cout << zad1(22, 8);
+    cout << "Zadanie 1 - MAX\n";
+    cout << zad1(22, 8) << endl;
 
+    cout << "Zadanie 2 - MIN\n";
+    cout << zad2(9, 8, 2) << endl;
+
+    cout << "Zadanie 3 - IF\n";
+    cout << zad3(9, 12) << endl;
+
+    cout << "Zadanie 4 - MACIERZ\n";
+    int n = 4;
+    int** tab1 = new int* [n];
+    for (int i = 0; i < n; i++)
+        tab1[i] = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            tab1[i][j] = (i + j + 6);
+            cout << tab1[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    zad4(tab1, n);
+    cout << "Wynik: \n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << tab1[i][j] << "\t";
+        cout << endl;
+    }
+
+    cout << "Zadanie 3 - IF 2\n";
+    cout << zad5(2, 2) << endl;
 }
--- Semestr-4/Programowanie-niskopoziomowe/Lab07_CMOVcc/asm.asm
+++ Semestr-4/Programowanie-niskopoziomowe/Lab07_CMOVcc/asm.asm
@@ -25,4 +25,75 @@ zad1 proc
 	ret
 zad1 endp
 
+zad2 proc uses rbx ; rcx - a, rdx - b, r8 - c
+	cmp rcx, rdx
+	cmovle rbx, rcx
+	cmovnle rbx, rdx
+
+
+	cmp rbx, r8
+	cmovle rax, rbx
+	cmovnle rax, r8
+
+	ret
+zad2 endp
+
+zad3 proc uses rbx; rcx - a, rdx - b
+	mov r8, rcx
+	sub r8, rdx
+	
+	mov r9, rdx
+	sub r9, rcx
+
+	cmp rcx, rdx
+	cmovg rax, r8
+	cmovng rax, r9
+
+	ret
+zad3 endp
+
+zad4 proc uses rbx rsi rdi ; rcx - **tab, rdx - n
+	
+	mov rdi, rdx
+
+	petla1:
+		mov rsi, [rcx + 8*rdi - 8]
+		mov r8, rdx
+		petla2:
+			mov rax, 0
+			movsxd r9, dword ptr [rsi + 4*r8 - 4]
+			cmp r9, 10
+			cmovge r9d, eax
+			mov [rsi + 4*r8 - 4], r9d
+
+			dec r8
+		jnz petla2
+		dec rdi
+	jnz petla1
+
+	ret
+zad4 endp
+
+zad5 proc uses rbx rdi rsi; rcx - a, rdx - b
+	mov rsi, rcx ; a
+	mov rdi, rdx ; b
+	
+	mov rax, rsi
+	inc rax
+	xor rdx, rdx
+	div rdi
+	mov r8, rax ; wynik 1
+
+	mov r9, rdi
+	inc r9
+	imul r9, rsi
+
+
+	cmp rsi, rdi
+	cmovle rax, r8
+	cmovnle rax, r9
+
+	ret
+zad5 endp
+
 END
