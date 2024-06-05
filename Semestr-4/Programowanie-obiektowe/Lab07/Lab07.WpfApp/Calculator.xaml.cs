622f99871c26779492945a7494edbc21d7d9a11f
Łukasz <lukasz.zawodz@gmail.com>
2024-06-05 22:44:09 +0200

    PO lab07 - 10

---
+++ Semestr-4/Programowanie-obiektowe/Lab07/Lab07.WpfApp/Calculator.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lab07.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        public double value1;
+        public double result;
+        public char op;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button Btn)
+                PropText.Text += Btn.Content;
+        }
+
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button Btn)
+                PropText.Text = "";
+            result = 0;
+            value1 = 0;
+        }
+
+        private void Button_Add(object sender, RoutedEventArgs e)
+        {
+            value1 = double.Parse(PropText.Text);
+            PropText.Text = "";
+            op = 'a';
+        }
+        private void Button_Sub(object sender, RoutedEventArgs e)
+        {
+            value1 = double.Parse(PropText.Text);
+            PropText.Text = "";
+            op = 's';
+        }
+
+        private void Button_Mul(object sender, RoutedEventArgs e)
+        {
+            value1 = double.Parse(PropText.Text);
+            PropText.Text = "";
+            op = 'm';
+        }
+
+        private void Button_Div(object sender, RoutedEventArgs e)
+        {
+            value1 = double.Parse(PropText.Text);
+            PropText.Text = "";
+            op = 'd';
+        }
+
+        private void Button_Solve(object sender, RoutedEventArgs e)
+        {
+            switch (op)
+            {
+                case 'a':
+                    result = value1 + double.Parse(PropText.Text);
+                    PropText.Text = result.ToString();
+                    break;
+
+                case 's':
+                    result = value1 - double.Parse(PropText.Text);
+                    PropText.Text = result.ToString();
+                    break;
+                case 'm':
+                    result = value1 * double.Parse(PropText.Text);
+                    PropText.Text = result.ToString();
+                    break;
+                case 'd':
+                    result = value1 / double.Parse(PropText.Text);
+                    PropText.Text = result.ToString();
+                    break;
+            }
+        }
+    }
+}
---
+++ Semestr-4/Programowanie-obiektowe/Lab08/Lab08.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Lab08.DLL;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Lab08.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IList<Student> Students { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Students = new List<Student>()
+            {
+                new Student() { FirstName = "Adam", SurName = "Kowalski", Fauclty="WIMII", StudentNo = "1010"},
+                new Student() { FirstName = "Jan", SurName = "Adamski", Fauclty="WIMII", StudentNo = "1011"}
+            };
+
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "First Name", Binding = new Binding("FirstName") });
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "Sur Name", Binding = new Binding("SurName") });
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "Fauclty", Binding = new Binding("Fauclty") });
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "Number", Binding = new Binding("StudentNo") });
+            //DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "Sur Name", Binding = new Binding("Grades") });
+
+
+            DataGridStudents.AutoGenerateColumns = false;
+            DataGridStudents.ItemsSource = Students;
+        }
+
+        private void AddStudentWindow_click(object sender, RoutedEventArgs e)
+        {
+            AddStudentWindow asw = new AddStudentWindow();
+
+            if (asw.ShowDialog().Value)
+            {
+                Students.Add(asw.Student);
+                DataGridStudents.Items.Refresh();
+            }
+
+        }
+        private void DelStudent_click(object sender, RoutedEventArgs e)
+        {
+            if (DataGridStudents.SelectedItem is Student studentToDel)
+            {
+                Students.Remove(studentToDel);
+                DataGridStudents.Items.Refresh();
+            }
+        }
+
+        private void Btn_SaveTxt_Click(object sender, RoutedEventArgs e)
+        {
+            FileStream fs = new FileStream("data.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+
+            foreach (Student student in Students)
+            {
+                sw.WriteLine("[[Student]]");
+
+                sw.WriteLine("[Firstname]");
+                sw.WriteLine(student.FirstName);
+                sw.WriteLine("[Surname]");
+                sw.WriteLine(student.SurName);
+                sw.WriteLine("[Fauclty]");
+                sw.WriteLine(student.Fauclty);
+                sw.WriteLine("[ID Number]");
+                sw.WriteLine(student.StudentNo);
+            }
+
+
+
+            sw.Close();
+        }
+
+
+        private void Btn_LoadTxt_Click(object sender, RoutedEventArgs e)
+        {
+            FileStream fs = new FileStream("data.txt", FileMode.Create);
+            StreamReader sr = new StreamReader(fs);
+
+
+            while (!sr.EndOfStream)
+            {
+                var ln = sr.ReadLine();
+            }
+
+            DataGridStudents.Items.Refresh();
+
+            sr.Close();
+        }
+    }
+}
