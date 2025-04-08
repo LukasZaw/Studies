f3473d84b13946486f8e93211957601893b87b4e
Łukasz <lukasz.zawodz@gmail.com>
2025-04-08 20:31:37 +0200

    Lab05 - CSS keyframes animations for image gallery

--- Semestr-6/Interaktywna-grafika-i-prezentacja-danych/Lab04/script.js
+++ Semestr-6/Interaktywna-grafika-i-prezentacja-danych/Lab04/script.js
@@ -1,41 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const thumbnails = document.querySelectorAll(".thumbnail");
-  const lightbox = document.querySelector(".lightbox");
-  const lightboxImage = document.getElementById("lightbox-image");
+    const thumbnails = document.querySelectorAll(".thumbnail");
+    const lightbox = document.querySelector(".lightbox");
+    const lightboxImage = document.getElementById("lightbox-image");
+  
+    thumbnails.forEach((thumbnail) => {
+      thumbnail.addEventListener("click", () => {
+        const imageSrc = thumbnail.getAttribute("data-image");
+        lightboxImage.src = imageSrc;
+        lightbox.classList.add("active");
+  
+        if (["img/img1.jpg", "img/img2.jpg"].includes(imageSrc)) {
+          lightboxImage.classList.add("rotate");
+        } else {
+          lightboxImage.classList.remove("rotate");
+        }
+  
+        if (imageSrc === "img/img3.jpg") {
+          lightboxImage.classList.add("bounce");
+        } else {
+          lightboxImage.classList.remove("bounce");
+        }
 
-  thumbnails.forEach((thumbnail) => {
-    thumbnail.addEventListener("click", () => {
-      const imageSrc = thumbnail.getAttribute("data-image");
-      lightboxImage.src = imageSrc;
-      lightbox.classList.add("active");
+        if (["img/img4.jpg", "img/img5.jpg", "img/img6.jpg" , "img/img7.jpg"].includes(imageSrc)) {
+            lightboxImage.classList.add("slide-in-elliptic-bottom-fwd");
+          } else {
+            lightboxImage.classList.remove("slide-in-elliptic-bottom-fwd");
+          }
 
-      if (["img/img1.jpg", "img/img2.jpg"].includes(imageSrc)) {
-        lightboxImage.classList.add("rotate");
-      } else {
-        lightboxImage.classList.remove("rotate");
-      }
+          if (["img/img8.jpg", "img/img9.jpg", "img/img10.jpg"].includes(imageSrc)) {
+            lightboxImage.classList.add("roll-in-blurred-left");
+          } else {
+            lightboxImage.classList.remove("roll-in-blurred-left");
+          }
+  
+        setTimeout(() => {
+          lightboxImage.style.transform = "scale(1) translateY(0)";
+          lightboxImage.style.opacity = "1";
+        }, 50);
+      });
+    });
+  
+    lightbox.addEventListener("click", () => {
+      lightboxImage.classList.remove("rotate");
+      lightboxImage.classList.remove("bounce");
+      lightboxImage.classList.remove("slide-in-elliptic-bottom-fwd");
+      lightboxImage.classList.remove("roll-in-blurred-left");
 
-      if (imageSrc === "img/img3.jpg") {
-        lightboxImage.classList.add("bounce");
-      } else {
-        lightboxImage.classList.remove("bounce");
-      }
+      lightboxImage.classList.add("slide-out-elliptic-top-bck");
 
+      lightboxImage.style.opacity = "0";
       setTimeout(() => {
-        lightboxImage.style.transform = "scale(1) translateY(0)";
-        lightboxImage.style.opacity = "1";
-      }, 50);
+        lightbox.classList.remove("active");
+        lightboxImage.src = "";
+      }, 1100);
     });
   });
-
-  lightbox.addEventListener("click", () => {
-    lightboxImage.classList.remove("rotate");
-    lightboxImage.classList.remove("bounce");
-    lightboxImage.style.transform = "scale(0.5) translateY(-50px)";
-    lightboxImage.style.opacity = "0";
-    setTimeout(() => {
-      lightbox.classList.remove("active");
-      lightboxImage.src = "";
-    }, 500);
-  });
-});
---
+++ Semestr-6/Interaktywna-grafika-i-prezentacja-danych/Lab05/script.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const thumbnails = document.querySelectorAll(".thumbnail");
+
+  thumbnails.forEach((thumbnail) => {
+    const randomDelay = Math.random() * 3; // Random delay between 0 and 2 seconds
+    const randomX = (Math.random() - 0.5) * 200 + "vw"; // Random X offset (-100vw to 100vw)
+    const randomY = (Math.random() - 0.5) * 200 + "vh"; // Random Y offset (-100vh to 100vh)
+
+    thumbnail.style.animationDelay = `${randomDelay}s`;
+    thumbnail.style.transform = `translate(${randomX}, ${randomY})`;
+  });
+
+  const lightbox = document.querySelector(".lightbox");
+  const lightboxImage = document.getElementById("lightbox-image");
+
+  thumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", () => {
+      const imageSrc = thumbnail.getAttribute("data-image");
+      lightboxImage.src = imageSrc;
+      lightbox.classList.add("active");
+
+      if (["img/img1.jpg", "img/img2.jpg"].includes(imageSrc)) {
+        lightboxImage.classList.add("rotate");
+      } else {
+        lightboxImage.classList.remove("rotate");
+      }
+
+      if (imageSrc === "img/img3.jpg") {
+        lightboxImage.classList.add("bounce");
+      } else {
+        lightboxImage.classList.remove("bounce");
+      }
+
+      if (
+        [
+          "img/img4.jpg",
+          "img/img5.jpg",
+          "img/img6.jpg",
+          "img/img7.jpg",
+        ].includes(imageSrc)
+      ) {
+        lightboxImage.classList.add("slide-in-elliptic-bottom-fwd");
+      } else {
+        lightboxImage.classList.remove("slide-in-elliptic-bottom-fwd");
+      }
+
+      if (
+        ["img/img8.jpg", "img/img9.jpg", "img/img10.jpg"].includes(imageSrc)
+      ) {
+        lightboxImage.classList.add("roll-in-blurred-left");
+      } else {
+        lightboxImage.classList.remove("roll-in-blurred-left");
+      }
+
+      setTimeout(() => {
+        lightboxImage.style.transform = "scale(1) translateY(0)";
+        lightboxImage.style.opacity = "1";
+      }, 50);
+    });
+  });
+
+  lightbox.addEventListener("click", () => {
+    lightboxImage.classList.remove("rotate");
+    lightboxImage.classList.remove("bounce");
+    lightboxImage.classList.remove("slide-in-elliptic-bottom-fwd");
+    lightboxImage.classList.remove("roll-in-blurred-left");
+
+    lightboxImage.classList.add("slide-out-elliptic-top-bck");
+
+    lightboxImage.style.opacity = "0";
+    setTimeout(() => {
+      lightbox.classList.remove("active");
+      lightboxImage.src = "";
+    }, 1100);
+  });
+});
