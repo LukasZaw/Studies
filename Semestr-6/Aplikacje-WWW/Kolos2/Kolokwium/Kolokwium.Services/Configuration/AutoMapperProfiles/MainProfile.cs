0a5fc4fd0130db324e8c85c426fb92139d340636
Łukasz <lukasz.zawodz@gmail.com>
2025-06-07 22:03:31 +0200

    Awww Kolos2

---
+++ Semestr-6/Aplikacje-WWW/Kolos2/Kolokwium/Kolokwium.Services/Configuration/AutoMapperProfiles/MainProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Kolokwium.Model.DataModels;
+using Kolokwium.ViewModel.VM;
+
+namespace Kolokwium.Services.Configuration.AutoMapperProfiles;
+public class MainProfile : Profile
+{
+    public MainProfile()
+    {
+       CreateMap<Ksiazka, KsiazkiVm>()
+            .ForMember(dest => dest.ImieNazwiskoAutora,
+                opt => opt.MapFrom(src => src.Autor.Imie + " " + src.Autor.Nazwisko));
+            CreateMap<AddKsiazkaVm, Ksiazka>();
+
+    }
+}
+
---
+++ Semestr-6/Aplikacje-WWW/Kolos2/Kolokwium/Kolokwium.ViewModel/VM/KsiazkaVm.cs
@@ -0,0 +1,12 @@
+namespace Kolokwium.ViewModel.VM
+{
+    public class KsiazkiVm
+    {
+        public int Id { get; set; }
+        public string Tytul { get; set; } = string.Empty;
+        public string Wydawnictwo { get; set; } = string.Empty;
+        public int RokWydania { get; set; }
+        public string ImieNazwiskoAutora { get; set; } = string.Empty;
+        
+    }
+}
